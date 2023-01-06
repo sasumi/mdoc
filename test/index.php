9be81d3972bc9796b96648de68aa7c64ebe9f2aa
sasumi <cnsasumi@gmail.com>
2023-01-06 11:03:29 +0800

    upd

--- test/index.php
+++ test/index.php
@@ -1,6 +1,6 @@
 <?php
 use function Lfphp\Mdoc\setup_mdoc;
-include_once __DIR__.'/../release/mdoc.phar';
+include_once __DIR__.'/../src/bootstrap.php';
 setup_mdoc([
 	'root' => __DIR__.'/document',
 ]);
